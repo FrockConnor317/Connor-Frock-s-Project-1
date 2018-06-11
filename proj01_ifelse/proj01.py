e94c51e6d273cd330f1c2a56f07c6f662433f254
FrockConnor317 <39972345+FrockConnor317@users.noreply.github.com>
2018-06-11 14:34:48 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -7,9 +7,20 @@
 # This program asks the user for his/her name and grade.
 #Then, it prints out a sentence that says the number of years until they graduate.
 
+Name = raw_input('What is your name?')
+Grade = int(raw_input("what grade are you in?"))
+print  str(Name[0].upper() + Name[1:6].lower()) + ",you will graduate from high school in " + str(13- Grade) + " years"
 # Part II:
 # This program asks the user for his/her name and birth month.
 # Then, it prints a sentence that says the number of days and months until their birthday
+
+current_day = 11
+current_month= 6
+user_month = int(raw_input("Enter the number of your birth month: "))
+user_day = int(raw_input("enter the number of your birth day: "))
+if user_month >= current_month
+    str(user_month - current_month)
+else: str()
 
 
 # If you complete extensions, describe your extensions here!
