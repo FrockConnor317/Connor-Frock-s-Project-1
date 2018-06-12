fbba0875c6fe5dd6d705536d33a4289f690f9a17
FrockConnor317 <39972345+FrockConnor317@users.noreply.github.com>
2018-06-12 09:54:18 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -14,7 +14,7 @@ print  str(Name[0].upper() + Name[1:6].lower()) + ",you will graduate from high 
 # This program asks the user for his/her name and birth month.
 # Then, it prints a sentence that says the number of days and months until their birthday
 
-current_day = 11
+current_day = 12
 current_month= 6
 user_month = int(raw_input("Enter the number of your birth month: "))
 user_day = int(raw_input("enter the number of your birth day: "))
@@ -30,6 +30,28 @@ if user_day >= current_day:
    print str(b) + " days until birthday"
 else:
     print str(d) + "days until birthday"
+
+Age = raw_input("what's your age?")
+if int(Age) < 13:
+    print " you are only allowed to see PG and G movies"
+elif int(Age) >= 13 and int(Age) < 17:
+    print " you are allowed to see pg-13, PG, and G movies"
+else:
+    print "you are allowed to watch movies of all ratings"
+
+movie = raw_input("do you want to watch a movie sometime?")
+if movie == "yes":
+    print "is tomorrow at 6:30 fine for you?"
+
+else:
+    print " alright then, ill take my spicy memes elsewhere "
+
+
+
+
+
+
+
 
 
 
